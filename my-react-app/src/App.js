a7686d2ee2a8be2e9a90bb1e39d78f6af9e7f83d
mohtashim5 <mouhtashim5@gmail.com>
2019-03-08 23:46:42 +0500

    LifeCycle Hooks

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,29 +1,30 @@
-import React, { Component, Fragment  } from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import Users from '../src/user/users';
 // import User from '../src/user/user'
 // import Child from './components/parentToChild/Child';
 // import Parent from './components/parentToChild/parent';
 // import User from './component/user'
+import Child from './Child'
 import './App.css';
-import uniqueId from 'react-html-id';
+// import uniqueId from 'react-html-id';
 
 
 
 
-const User = (props) => {
-  return (
-      <Fragment>
-          {/* <div key="1">hi</div>
-          <div key="1">hello</div> */}
-          {
-            props.greetings === 'hi'
-            ? `&lt;div&gt;${props.greetings}^slakmx`
-            : props.greetings
-          }
-          </Fragment>
-  )
-}
+// const User = (props) => {
+//   return (
+//       <Fragment>
+//           {/* <div key="1">hi</div>
+//           <div key="1">hello</div> */}
+//           {
+//             props.greetings === 'hi'
+//             ? `&lt;div&gt;${props.greetings}^slakmx`
+//             : props.greetings
+//           }
+//           </Fragment>
+//   )
+// }
 class App extends Component {
   //   state = {
   //     name: 'techsith'
@@ -78,22 +79,41 @@ class App extends Component {
   // changeUserName = (id, e) => {
   //   debugger
   //   this.setState({[e.target.name]: e.target.value})
-    // const index = this.state.users.findIndex((user) => {
-    //   console.log(index)
-    //   return user.id === id
-    // });
-    // const user = Object.assign({}, this.state.users[index]);
-    // user.name = e.target.value;
-    // const users = Object.assign.name([], this.state.users);
-    // users[index] = user;
-    // this.setState({ users: users });
- 
+  // const index = this.state.users.findIndex((user) => {
+  //   console.log(index)
+  //   return user.id === id
+  // });
+  // const user = Object.assign({}, this.state.users[index]);
+  // user.name = e.target.value;
+  // const users = Object.assign.name([], this.state.users);
+  // users[index] = user;
+  // this.setState({ users: users });
 
+  constructor() {
+    super();
+    this.state = {
+      name: 'peter'
+    };
+    console.log('construtor')
+  }
+  HandleChange = () =>{
+    debugger
+    this.setState({ name: 'Devit' })
+  }
+  componentWillMount() {
+    if (window.innerWidth < 500) {
+      this.setState({ innerWidth: window.innerWidth });
+    }
+    console.log('componentWillMount')
+  }
 
   render() {
     return (
       <div className="App">
-        <User greetings = "hi"/>
+        name:{this.state.name}
+        | innerWidth: {this.state.innerWidth}
+        <Child name={this.state.name} />
+        <button onClick={() => this.HandleChange()}>Change State</button>
       </div>
     )
   }
---
+++ my-react-app/src/Child.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Child extends Component {
+    constructor() {
+        super();
+        console.log('construtor')
+    }
+
+    componentWillMount() {
+        console.log('child componentWillMount');
+    }
+    componentDidMount() {
+        console.log('Child componentDidMount')
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('Child shouldComponentUpdate')
+        return false;
+    }
+    componentWillReceiveProps() {
+        console.log('Child componentWillReceiveProps')
+    }
+
+    render() {
+        console.log('child render')
+        return (
+            <div className="App">
+                child Name:{this.props.name}
+            </div>
+        )
+    }
+}
+
+export default Child;
