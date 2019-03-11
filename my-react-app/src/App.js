03b412882c108751228153ef31171565ceabef3e
mohtashim5 <mouhtashim5@gmail.com>
2019-03-11 12:53:15 +0500

    refs

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -11,34 +11,38 @@ import Route from 'react-router-dom/Route';
 import { BrowserRouter as Router } from 'react-router-dom';
 // import uniqueId from 'react-html-id';
 
-const User =({match}) =>{
-  return(<h1>React {match.params.username}</h1>)
-}
+
 
 class App extends Component {
+  submit = () => {
+    console.log(this.firstName.value);
+    console.log(this.lastName.value);
+    console.log(this.age.value);
+  }
+  onKeyUp = (e) => {
+    console.log(e.keyCode)
+    if(e.keyCode === 13){
+      this.lastName.focus();
+      
+    }
+  }
   render() {
     return (
-      <Router>
-        <div className="App">
-          
-          <Route path="/" exact strict
-          render={
-            () => {
-              return(<h1>Wellcome Home</h1>)
-            }
-          }/>
-          
-          <Route path="/about/" exact strict
-          render={
-            () => {
-              return(<h1>Wellcome About</h1>)
-            }
-          }/>
-
-          <Route path="/user/:username" exact strict component={User}
-          />
+      <div className="App">
+        <h1>Form</h1>
+        <div>
+          <span>First Name:</span>
+          <input ref={(input) => { this.firstName = input }} onKeyUp={()=>this.onKeyUp} type="text" />
         </div>
-      </Router>
+        <div>
+          <span>Last Name:</span>
+          <input ref={(input) => { this.lastName = input }} type="text" />
+        </div>
+        <div>
+          <span>Age:</span>
+          <input ref={(input) => { this.age = input }} type="submit" value="submit" onClick={this.submit} />
+        </div>
+      </div>
     );
   }
 }
